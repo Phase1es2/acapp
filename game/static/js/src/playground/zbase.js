50d6bf226752e497709be4d80e45f15f687a9865
Phase1es2 <hyang17@uw.edu>
2023-11-19 11:59:09 +0800

    first game

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,10 +1,21 @@
 class AcGamePlayground {
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>PlayGround</div>`);
-        
-        this.hide();
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
+
+        //this.hide();
         this.root.$ac_game.append(this.$playground);
+
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
+
+        for(let i = 0; i < 5; i++) {
+
+            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+        }
 
         this.start();
     }
@@ -12,6 +23,15 @@ class AcGamePlayground {
     start() {
     }
 
+    get_random_color() {
+        let r, g, b;
+        do {
+            r = Math.floor(Math.random() * 256);
+            g = Math.floor(Math.random() * 256);
+            b = Math.floor(Math.random() * 256);
+        } while(r + g + b > 700);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
 
 
     show() { //open playground
