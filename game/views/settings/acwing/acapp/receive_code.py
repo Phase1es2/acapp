5bd096486ca87a590e7583a7ef41a2405a258871
Phase1es2 <hyang17@uw.edu>
2023-11-22 04:30:49 +0800

    AcWing APP OAuth2 Login

--- game/views/settings/acwing/acapp/receive_code.py
+++ game/views/settings/acwing/acapp/receive_code.py
@@ -1,19 +1,28 @@
-from django.shortcuts import redirect
+from django.http import JsonResponse
 from django.core.cache import cache
 import requests
 from django.contrib.auth.models import User
 from game.models.player.player import Player
-from django.contrib.auth import login
 from random import randint
 
+
 def receive_code(request):
-    data = request.GET;
+    data = request.GET
+
+    if "errcode" in data:
+        return JsonResponse({
+            'result': "apply failed",
+            'errcode': data['errcode'],
+            'errmsg': data['errmsg'],
+        })
+
     code = data.get('code')
     state = data.get('state')
 
     if not cache.has_key(state):
-        return redirect("index")
-    
+        return JsonResponse({
+            'result': "state not exist"
+        })
     cache.delete(state)
 
     apply_access_token_url = "https://www.acwing.com/third_party/api/oauth2/access_token/"
@@ -28,28 +37,34 @@ def receive_code(request):
     access_token = access_token_res['access_token']
     openid = access_token_res['openid']
 
-
     players = Player.objects.filter(openid=openid)
     if players.exists():
-        login(request, players[0].user)
-        return redirect("index")
+        player = players[0]
+        return JsonResponse({
+            'result': "success",
+            'username': player.user.username,
+            'photo': player.photo,
+        })
+
 
     get_userinfo_url = "https://www.acwing.com/third_party/api/meta/identity/getinfo/"
     params = {
-        'access_token': access_token,
-        'openid': openid,
+        "access_token": access_token,
+        "openid": openid
     }
-    
     userinfo_res = requests.get(get_userinfo_url, params=params).json()
     username = userinfo_res['username']
     photo = userinfo_res['photo']
 
-    while User.objects.filter(username=username).exists():
-        username ++str(randint(0, 9))
+    while User.objects.filter(username=username).exists():  #
+        username += str(randint(0, 9))
 
     user = User.objects.create(username=username)
     player = Player.objects.create(user=user, photo=photo, openid=openid)
 
-    login(request, user)
-    
-    return redirect("index")
+    return JsonResponse({
+        'result': "success",
+        'username': player.user.username,
+        'photo': player.photo,
+    })
+
